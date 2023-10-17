9d99bca237051e1335a15efca50c3d571f9c3fc6
Kevin <95513702+TravelerSW@users.noreply.github.com>
2023-10-17 20:42:46 -0300

    Implementação da camada de domínio.

--- src/main/java/me/dio/BootcampJavaRestfulApplication.java
+++ src/main/java/me/dio/BootcampJavaRestfulApplication.java
@@ -1,5 +1,7 @@
 package me.dio;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootcampJavaRestfulApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootcampJavaRestfulApplication.class, args);
-	}
-
+		SpringApplication app = new SpringApplication(BootcampJavaRestfulApplication.class);
+        //Adição do perfil "dev" para execução correta do código.
+        app.setAdditionalProfiles("dev");
+        app.run(args);    
+    }
 }
+
+
