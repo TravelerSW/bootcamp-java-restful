eb1ad0eb94a73a01bfcd4cefc3f3aaae8d30b2a4
Kevin <95513702+TravelerSW@users.noreply.github.com>
2023-10-19 21:55:32 -0300

    Conexão com o banco de dados na nuvem.

--- src/main/java/me/dio/BootcampJavaRestfulApplication.java
+++ src/main/java/me/dio/BootcampJavaRestfulApplication.java
@@ -10,8 +10,6 @@ public class BootcampJavaRestfulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(BootcampJavaRestfulApplication.class);
-        //Adição do perfil "dev" para execução correta do código.
-        app.setAdditionalProfiles("dev");
         app.run(args);    
     }
 }
